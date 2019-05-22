0db58a5f54c36af650819b1f07b916bf5ec61efc
supernebula <supernebula@126.com>
2019-05-22 18:38:42 +0800

    init

---
+++ yelp-config-server/src/main/java/com/evol/config/YelpConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.evol.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YelpConfigServerApplication.class, args);
+    }
+
+}
---
+++ yelp-eureka-server/src/main/java/com/evol/eureka/YelpEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.evol.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YelpEurekaServerApplication.class, args);
+    }
+
+}
