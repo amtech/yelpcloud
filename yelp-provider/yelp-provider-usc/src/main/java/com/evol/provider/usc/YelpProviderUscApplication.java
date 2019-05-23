52d917ba0ffb542e6c8f16739c42311023c5ace9
supernebula <supernebula@126.com>
2019-05-23 11:33:13 +0800

    init

--- yelp-provider/yelp-provider-usc/src/main/java/com/evol/provider/usc/YelpProviderUscApplication.java
+++ yelp-provider/yelp-provider-usc/src/main/java/com/evol/provider/usc/YelpProviderUscApplication.java
@@ -2,7 +2,9 @@ package com.evol.provider.usc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient //从Edgware开始，该注解可省略
 @SpringBootApplication
 public class YelpProviderUscApplication {
 
