54a5d78fd7a6f24bed6cd30dd38759f78bf955fc
supernebula <supernebula@126.com>
2019-08-06 18:37:26 +0800

    yelp-provider-usc 启用双节点集群，web-manage（consumer）使用ribbon通过eureka 轮询访问usc

--- yelp-provider/yelp-provider-usc/src/main/java/com/evol/provider/web/controller/UserController.java
+++ yelp-provider/yelp-provider-usc/src/main/java/com/evol/provider/web/controller/UserController.java
@@ -6,16 +6,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RequestMapping("/users")
 @RestController
 public class UserController {
 
     @GetMapping("find/{id}")
-    public User findById(@PathVariable Long id){
+    public User findById(HttpServletRequest httpReq, @PathVariable Long id){
         User user = new User();
         user.setId(id);
         user.setNick("zhagnsan" + id);
         user.setUsername("name_zhangshan" + id);
+        String url = httpReq.getRequestURL().toString();
+        String addr = httpReq.getRemoteAddr();
+        System.out.println(String.format("request url : %s, addr : %s", url, addr));
         return user;
     }
 }
