2f8f1c3be332612f18b2e07c682b4b168d2c4ff2
supernebula <supernebula@126.com>
2019-08-06 19:09:17 +0800

    application.yml 属性配置ribbon, 属性配置的优先级高于RibbonConfig代码配置 yelp-provider-usc:   ribbon:   NFLoadBalancerRuleClassName: com.netflix.loadbalancer.RandomRule

--- yelp-client/yelp-web-manage/src/main/java/com/config/RibbonConfig.java
+++ yelp-client/yelp-web-manage/src/main/java/com/config/RibbonConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
  * 该类为Ribbon的配置类
  * 注意：该类不能放在主应用程序上下文@ComponentScan所扫描的包中，否则配置将会被所有Ribbon Client共享。
  */
-@Configuration
+//@Configuration
 public class RibbonConfig {
     @Bean
     public IRule ribbonRule() {
--- yelp-client/yelp-web-manage/src/main/java/com/evol/manage/config/TestRibbonCustomConfig.java
+++ yelp-client/yelp-web-manage/src/main/java/com/evol/manage/config/TestRibbonCustomConfig.java
@@ -4,8 +4,8 @@ import com.config.RibbonConfig;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@RibbonClient(name = "yelp-provider-usc", configuration = RibbonConfig.class)
+//@Configuration
+//@RibbonClient(name = "yelp-provider-usc", configuration = RibbonConfig.class)
 public class TestRibbonCustomConfig {
 
 }
