ebbb6c08523c4caa880f069a80e996a13272e72d
supernebula <supernebula@126.com>
2019-08-06 20:13:41 +0800

    Ribbon配置自定义全局配置，代码配置OK,属性配置OK; ribbon属性配置饥饿加载 未实现

--- yelp-client/yelp-web-manage/src/main/java/com/evol/manage/config/RibbonClientDefaultConfigurationTestsConfig.java
+++ yelp-client/yelp-web-manage/src/main/java/com/evol/manage/config/RibbonClientDefaultConfigurationTestsConfig.java
@@ -3,6 +3,6 @@ package com.evol.manage.config;
 import com.config.DefaultRibbonConfig;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
-@RibbonClients(defaultConfiguration = DefaultRibbonConfig.class)
+//@RibbonClients(defaultConfiguration = DefaultRibbonConfig.class)
 public class RibbonClientDefaultConfigurationTestsConfig {
 }
