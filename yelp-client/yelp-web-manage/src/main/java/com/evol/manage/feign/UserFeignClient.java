f08c4e8b01ec33b5019eb963f11701250d1e4e3d
supernebula <supernebula@126.com>
2019-08-07 18:23:34 +0800

    feign打印日志，代码配置OK

--- yelp-client/yelp-web-manage/src/main/java/com/evol/manage/feign/UserFeignClient.java
+++ yelp-client/yelp-web-manage/src/main/java/com/evol/manage/feign/UserFeignClient.java
@@ -1,13 +1,31 @@
 package com.evol.manage.feign;
 
 import com.evol.manage.model.User;
+import feign.Logger;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "yelp-provider-usc")
+@FeignClient(name = "yelp-provider-usc", configuration = UserFeignClient.UserFeignConfig.class)
 public interface UserFeignClient {
 
     @GetMapping("/users/find/{id}")
     User findById(@PathVariable("id") Long id);
+
+    /**
+     * 该Feign Client的配置类，注意：
+     * 1. 该类可以独立出去；
+     * 2. 该类上也可添加@Configuration声明是一个配置类；
+     * 配置类上也可添加@Configuration注解，声明这是一个配置类；
+     * 但此时千万别将该放置在主应用程序上下文@ComponentScan所扫描的包中，
+     * 否则，该配置将会被所有Feign Client共享，无法实现细粒度配置！
+     * 个人建议：像我一样，不加@Configuration注解
+     */
+    class UserFeignConfig {
+        @Bean
+        public Logger.Level logger(){
+            return Logger.Level.FULL;
+        }
+    }
 }
