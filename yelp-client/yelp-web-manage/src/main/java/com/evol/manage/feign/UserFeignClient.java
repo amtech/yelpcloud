d6e858e52284675836a4d6fc8a5160c3018dd05c
supernebula <supernebula@126.com>
2019-08-10 19:35:18 +0800

    Feign使用Hystrix服务降级： 为Feign配置提供Fallback： @FeignClient(name = "yelp-provider-usc", fallback = UserFeignClientFallback.class)
    为Feigh配置提供获得造成fallbackFactory 的原因：
    @FeignClient(name = "yelp-provider-usc", fallbackFactory = UserFeignClientFallbackFactory.class)
    
    Feign全局启用/禁用Hystrix：
    feign.hystrix.enabled: true   #或false

--- yelp-client/yelp-web-manage/src/main/java/com/evol/manage/feign/UserFeignClient.java
+++ yelp-client/yelp-web-manage/src/main/java/com/evol/manage/feign/UserFeignClient.java
@@ -3,13 +3,17 @@ package com.evol.manage.feign;
 import com.evol.manage.model.User;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import feign.Logger;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "yelp-provider-usc", configuration = UserFeignClient.UserFeignConfig.class)
-@FeignClient(name = "yelp-provider-usc")
+//@FeignClient(name = "yelp-provider-usc", fallback = UserFeignClientFallback.class)
+@FeignClient(name = "yelp-provider-usc", fallbackFactory = UserFeignClientFallbackFactory.class)
 public interface UserFeignClient {
 
     @GetMapping("/users/find/{id}")
@@ -38,4 +42,52 @@ public interface UserFeignClient {
 //            return Logger.Level.FULL;
 //        }
 //    }
+}
+
+@Component
+class UserFeignClientFallback implements UserFeignClient{
+
+    @Override
+    public User findById(Long id){
+        return new User(id, "F默认用户1", "F默认密码1", "F默认昵称1");
+    }
+
+    @Override
+    public User findByPwd(String username, String password){
+        return new User(0L, "F默认用户3", "F默认密码3", "F默认昵称3");
+    }
+
+    @Override
+    public User getUser(Long  id){
+        return new User(id, "F默认用户4", "F默认密码4", "F默认昵称4");
+    }
+}
+
+
+@Component
+@Slf4j
+class UserFeignClientFallbackFactory implements FallbackFactory<UserFeignClient>{
+
+    public UserFeignClient create(Throwable throwable){
+        return new UserFeignClient() {
+            @Override
+            public User findById(Long id) {
+                log.error("进入回退逻辑",  throwable);
+                return new User(id,  "默认用户Factory", "默认密码Fac", "默认昵称Fac");
+            }
+
+            @Override
+            public User findByPwd(String username, String password) {
+                log.error("进入回退逻辑",  throwable);
+                return new User(0L,  "默认用户Factory", "默认密码Fac", "默认昵称Fac");
+            }
+
+            @Override
+            public User getUser(Long id) {
+                log.error("进入回退逻辑",  throwable);
+                return new User(id,  "默认用户Factory", "默认密码Fac", "默认昵称Fac");
+            }
+        };
+    }
+
 }
